39a7f238dba9547e89aad2a0d321dcbea1e377f7
Jiyun Park <jiyun@yolocorp.kr>
2022-02-21 20:06:31 +0900

    cors

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const fs = require('fs');
+app.use(cors())
 
 app.get('/', function(req, res) {
     res.send("^.^")
